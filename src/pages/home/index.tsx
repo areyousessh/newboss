93411e560ccb0c8fb5d7a24df06900d64d7c61f0
Sessh <gusalvescontato@gmail.com>
2023-10-07 15:22:53 -0300

    feat: start to refactor schedules form

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useContext, useEffect, useState} from "react";
-import { SafeAreaView, View, TouchableOpacity, FlatList, Image, Text, Dimensions, Linking, ScrollView } from "react-native";
+import { SafeAreaView, View, TouchableOpacity, FlatList, Image, Text, Dimensions, Linking} from "react-native";
 import { propsStack } from "../../routes/stack/models";
 import { Entypo, AntDesign, EvilIcons } from '@expo/vector-icons';
 import { Button } from "../../components/button";
@@ -9,15 +9,13 @@ import {useForm, Controller} from 'react-hook-form'
 import { AppContext } from "../../context/globalContext";
 import { Card, CardProps } from "../../components/card";
 import { Loading } from "../../components/loading";
-import { DayPicker } from "../../components/dayPicker";
-import { HoursPicker } from "../../components/hoursPicker";
 import SelectDropdown from "react-native-select-dropdown";
 import * as ImagePicker from "expo-image-picker"
 import Toast from "react-native-toast-message";
 import axios from "axios";
 import jwtDecode from 'jwt-decode'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import moment from "moment";
+
 
 type FormDataProps = {
     barberName: string;
@@ -37,14 +35,12 @@ export function Home() {
     const navigation = useNavigation<propsStack>()
     const [modalProfileVisible, setModalProfileVisible] = useState(false);
     const [modalScheduleVisible, setModalScheduleVisible] = useState(false);
-    const [selectedDays, setSelectedDays] = useState({})
+    const [image, setImage] = useState(null || undefined)
     const [userData, setUserData] = useState<UserDataProps>()
     const [isLoading, setIsLoading] = useState(false);
-    const [image, setImage] = useState(null || undefined)
     const [barberName, setBarberName] = useState('')
     const [schedules, setSchedules] = useState<CardProps[]>([])
     const [filteredSchedule, setFilteredSchedule] = useState<CardProps[]>([])
-    const [selectedTimes, setSelectedTimes] = useState<string[]>([]);
     const {control, handleSubmit} = useForm<FormDataProps>(); 
     const {tokenJwt, setTokenJwt} = useContext(AppContext)
     const barber = ["Bruno", "Mauricio", "Luiz"]
@@ -53,44 +49,7 @@ export function Home() {
     const decodedToken = jwtDecode(tokenJwt) as {userId: string}
     const id_user = decodedToken.userId
     const InstaURL =  'https://www.instagram.com/newbossbarbershop_'
-    const today = new Date();
-    const next30Days = [];
-    const formattedDates = []
-    const hours = ['09h00', '09h30', '10h00', '10h30', '11h00', '11h30', '12h00', '12h30', '13h00', '13h30', 
-                '14h00', '14h30', '15h00', '15h30', '16h00', '16h30', '17h00', '17h30', '18h00', '18h30', '19h00', '19h30', '20h00']
-    require('moment/locale/pt-br')
 
-    for (let i = 0; i < 30; i++) {
-        const nextDay = new Date(today)
-        nextDay.setDate(today.getDate() + i);
-        next30Days.push(nextDay)
-    }
-
-    for (const date of next30Days) {
-        const formattedDate = {
-            originalDate: next30Days,
-            dayOfWeek: moment(date).format('D'),
-            dayOfMonth: moment(date).format('ddd')
-        };
-        formattedDates.push(formattedDate)
-    }
-
-    function handleDaySelect(dayOfMonth, dayOfWeek, isSelected) {
-       const updatedSelectedDays = {
-        available: {dayOfMonth, dayOfWeek, isSelected},
-       };
-       setSelectedDays(updatedSelectedDays);
-       console.log(updatedSelectedDays)
-    }
-
-    function handleTimeSelect(time: string) {
-        if (selectedTimes.includes(time)) {
-            setSelectedTimes(selectedTimes.filter(selectedTimes => selectedTimes !== time));
-            console.log(selectedTimes)
-        } else {
-            setSelectedTimes([...selectedTimes, time]);
-        }
-    }
 
     function toggleModalProfile() {
         setModalProfileVisible(!modalProfileVisible)
@@ -153,50 +112,6 @@ export function Home() {
                 type: 'error',
                 text1: 'Erro ao abrir o link'
             })
-        }
-    }
-
-    async function handleSchedule(data: FormDataProps) {
-        const schedule = {...data, clientName: userData.name, id_user}
-        try {
-            const response = await axios.post('http://localhost:3001/create-schedule', schedule)
-            if (response.status === 200) {
-                Toast.show({
-                    type: "success",
-                    text1: 'Agendamento feito com sucesso !'
-                })
-            }
-        } catch (e: any) {
-            if (e.response.status === 500) {
-                Toast.show({
-                    type: "error",
-                    text1: 'Erro ao realizar o agendamento'
-                })
-            }
-        } finally {
-            getSchedules()
-            setModalScheduleVisible(false)
-        }
-    }
-
-    async function handleAvailableSchedules() {
-        const availableSchedules = {barberName, selectedDays, selectedTimes}
-        try {
-            const response = await axios.post('http://localhost:3001/available-schedules', availableSchedules)
-            if (response.status === 200) {
-                Toast.show({
-                    type: "success",
-                    text1: 'Horários disponiveis registrados'
-                })
-                setModalProfileVisible(false)
-            }
-        } catch (e: any) {
-            if (e.response.status === 500) {
-                Toast.show({
-                    type: "error",
-                    text1: 'Erro ao enviar os horarios disponiveis'
-                })
-            }
         }
     }
 
@@ -302,23 +217,6 @@ export function Home() {
                 />
                 { barberName !== '' ?
                 <>
-                    <View className="h-32 w-10/12 mt-10 items-center">
-                    <ScrollView horizontal>
-                        {formattedDates.map((date, index) => (
-                            <DayPicker key={index} dayOfWeek={date.dayOfWeek} dayOfMonth={date.dayOfMonth} onSelect={handleDaySelect}/>
-                        ))}
-                    </ScrollView>
-                </View> 
-                <View className="h-32 w-10/12 mt-10 items-center">
-                    <ScrollView horizontal>
-                        {hours.map((time, index) => (
-                            <HoursPicker key={index} time={time} selected={selectedTimes.includes(time)} onSelect={handleTimeSelect}/>
-                        ))}
-                    </ScrollView>        
-                </View>
-                <View>
-                    <Button type="large" title="Enviar Horários Disponiveis" onPress={() => handleAvailableSchedules()}/>
-                </View> 
                 </> 
                 : null }
                 </>
@@ -346,7 +244,7 @@ export function Home() {
             )}/>
             </View>
             <View className="items-center ios:mt-8 android:mt-6">
-                <Button onPress={handleSubmit(handleSchedule)} type="large" title="Agendar"/>
+                <Button type="large" title="Agendar"/>
             </View>
         </CustomModal>
     </SafeAreaView>
